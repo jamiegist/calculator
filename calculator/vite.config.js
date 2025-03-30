66c06160c82ec103e68b9a4191740092599965c6
jamie gist <jamie.gist.7@gmail.com>
2025-03-30 14:20:08 -0700

    base path

--- calculator/vite.config.js
+++ calculator/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/calculator-react/tree/main/calculator/src/vite.config.js",
+  base: process.env.VITE_BASE_PATH || "/calculator-react/tree/main/calculator/src/index.html",
 })
