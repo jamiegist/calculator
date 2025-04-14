b5c038f476b2ea5415d1e307c002cb01831c8d3d
jamie gist <jamie.gist.7@gmail.com>
2025-04-13 19:06:28 -0700

    minor changes

--- calculator/vite.config.js
+++ calculator/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/calculator-react/tree/main/calculator/src/index.html",
-})
+  plugins: [react()]
+});
